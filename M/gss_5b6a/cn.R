15cb03b0455d30e6ac43e14ed0aca476721e9596
Arni Magnusson <arnima@hafro.is>
2020-02-06 11:24:11 +0100

    Catage barplots

--- M/gss_5b6a/cn.R
+++ M/gss_5b6a/cn.R
@@ -5,6 +5,7 @@
 
 
 library(stockassessment)
+library(icesTAF)
 
 ## 1  Read catch-at-age data
 
@@ -20,25 +21,26 @@ c99 <- cn["1999",]
 ## 2  Plot data
 
 pdf("cn.pdf", width=14, height=8)
-
 par(mfrow=c(2,3))
-
 barplot(c95, main=1995)
 barplot(c96, main=1996)
 barplot(c97, main=1997)
 barplot(c98, main=1998)
 barplot(c99, main=1999)
-
 dev.off()
 
 ## 3  Plot all years
 
-## Zoom in on younger ages, evaluating what is an appropriate age at recruitment
-
-cn.young <- t(cn[,as.character(5:8)])
-barchart(cn.young, groups=rownames(cn), horizontal=FALSE, as.table=TRUE)
+pdf("catage-gss-faroes.pdf", width=14, height=8)
+barchart(t(cn[,as.character(5:18)]), groups=rownames(cn), horizontal=FALSE, as.table=TRUE)
+dev.off()
 
 ic <- read.ices("data/cn_intercatch.dat")
-ic.young <- t(ic[,as.character(2:8)])
+pdf("catage-gss-iceland.pdf", width=14, height=8)
+barchart(t(ic[,as.character(4:18)]), groups=rownames(ic), horizontal=FALSE, as.table=TRUE)
+dev.off()
 
-barchart(ic.young, groups=rownames(ic), horizontal=FALSE, as.table=TRUE)
+cod <- read.csv("COD_catch_at_age.csv", row.names=1, check.names=FALSE)[-(1:2)]
+pdf("catage-cod.pdf", width=14, height=8)
+barchart(t(cod), groups=rownames(cod), horizontal=FALSE, as.table=TRUE)
+dev.off()
